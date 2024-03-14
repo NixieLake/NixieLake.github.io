8246b458fd7130a4e1557eafbba1da6f151f300e
NixieLake <coffeestar1118@gmail.com>
2024-03-14 16:29:49 -0400

    Declare vars for filename and alt text lists.

--- labs/lab4/pt2/main.js
+++ labs/lab4/pt2/main.js
@@ -12,8 +12,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [
+  'images/pic1.jpg', 
+  'images/pic2.jpg', 
+  'images/pic3.jpg', 
+  'images/pic4.jpg', 
+  'images/pic5.jpg'
+];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+  'images/pic1.jpg': 'Closeup of a human eye',
+  'images/pic2.jpg': 'Rock that looks like a wave',
+  'images/pic3.jpg': 'Purple and white pansies',
+  'images/pic4.jpg': 'Section of wall from a pharoah\'s tomb',
+  'images/pic5.jpg': 'Large moth on a leaf'
+}
 
 /* Looping through images */
 
