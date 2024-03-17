7aebec9ab6ac69a2e1ab15ddc18e57903a689eca
NixieLake <coffeestar1118@gmail.com>
2024-03-17 13:23:21 -0400

    Add collision detection.

--- labs/lab4/pt3/main.js
+++ labs/lab4/pt3/main.js
@@ -66,6 +66,20 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 // create the balls
@@ -97,6 +111,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
